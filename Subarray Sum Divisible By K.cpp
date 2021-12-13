d03462059076c5010083ac1c381eb51c6b7f1402
ruchika1806 <62145263+ruchika1806@users.noreply.github.com>
2021-12-14 01:50:40 +0530

    Add files via upload

---
+++ Subarray Sum Divisible By K.cpp
@@ -0,0 +1,20 @@
+//Subarray Sum Divisible By K : LEETCODE-974
+
+//T.C : O(n) 
+//S.C : O(n)
+
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& A, int K) {
+        vector<int> counts(K, 0);
+        int sum = 0;
+        for(int x: A){
+            sum += (x%K + K)%K;
+            counts[sum % K]++;
+        }
+        int result = counts[0];
+        for(int c : counts)
+            result += (c*(c-1))/2;
+        return result;
+    }
+};
