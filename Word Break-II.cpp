e485217cd1b6bb9a46b7da3c0ea6abcf5da73e21
ruchika1806 <62145263+ruchika1806@users.noreply.github.com>
2022-01-24 18:42:15 +0530

    Add files via upload

---
+++ Word Break-II.cpp
@@ -0,0 +1,26 @@
+//Word Break -II - LEETCODE 140
+
+//T.C - 
+//S.C - O(n)
+
+class Solution {
+unordered_map<string, vector<string>> dp;
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        if(dp.find(s) != dp.end()) return dp[s];
+        vector<string> result;
+        for(string w : wordDict){
+            if(s.substr(0, w.length()) == w){
+                if(w.length() == s.length())
+                    result.push_back(w);
+                else{
+                    vector<string> tmp = wordBreak(s.substr(w.length()), wordDict);
+                    for(string t : tmp)
+                        result.push_back(w + " " + t);
+                }
+            }
+        }
+        dp[s] = result;
+        return result;
+    }
+};
